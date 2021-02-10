9551ec8a88fda491dfb7a88eb7bde97f8e814486
Philipp Seiler <ps@typoheads.at>
2021-02-10 14:28:12 +0100

    [#11673] Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redirects
+
+Provides additional functionality for dealing with redirects in TYPO3.
---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF['redirects'] = [
+    'title' => 'Redirects',
+    'description' => 'Provides additional functionality for dealing with redirects in TYPO3.',
+    'version' => '1.0.0',
+    'category' => 'module',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.2.0-0.0.0',
+            'typo3' => '9.0.0-10.9.9'
+        ]
+    ],
+    'state' => 'stable',
+    'author' => 'Dev-Team Typoheads',
+    'author_email' => 'hello@typoheads.at',
+    'author_company' => '',
+    'clearcacheonload' => true
+];
