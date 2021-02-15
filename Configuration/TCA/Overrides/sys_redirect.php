23ab719d6b8b05b08e5be3952401e753c15be495
Philipp Seiler <ps@typoheads.at>
2021-02-15 09:21:46 +0100

    Remove broken prefilling of source URL.

--- Configuration/TCA/Overrides/sys_redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-
-$GLOBALS['TCA']['sys_redirect']['columns']['source_path']['config']['type'] = 'prefillSourcePathFromRedirectManager';
